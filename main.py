5f6d752a62694cbe71635308e327d6a9aad0910a
xandeco codigokid <alepbravo1702@gmail.com>
2024-07-17 17:49:14 -0300

    a

--- main.py
+++ main.py
@@ -9,14 +9,30 @@ class AseTxtBtn(ft.ElevatedButton):
     def on_click(self):
         print("Clicked", self.text)
 
+def ToolsContainer():
+    pass
+
 def main(page: ft.Page):
     
-    btn = AseTxtBtn("Click me")
-    btn2 = ft.ElevatedButton("Click me")
-    page.add(btn)
-    page.add(btn2)
+    page.title = "Pygas"
 
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    controls = [
+        toolsContainer = ft.Container(
+            width=200,
+            height=1000,
+            bgcolor="red",
+            margin=0,
+        ),
 
+        mainContainer = ft.Container(
+            width="100",
+            height="100",
+            bgcolor="blue",
+            margin=0,
+        ),
+    ]
+
+    #page.add(toolsContainer)
+    page.add(controls)
 
 ft.app(main)
