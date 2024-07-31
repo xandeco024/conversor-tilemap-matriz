fc4dd4a5764eeaa13bff15864546e9e09d1827f4
Xandeco <>
2024-07-31 01:04:17 -0300

    ta indo!

--- mainagain.py
+++ mainagain.py
@@ -21,6 +21,48 @@ currentBrush = 0
 scale = 1
 offset = [0,0]
 
+hideTileText = False
+
+editing = False
+
+colors = (
+    (255, 255, 255),
+    (0, 255, 0),
+    #brown dirt color
+    (139, 69, 19),
+)
+
+# Functions
+
+def ToggleHideTileText():
+    global hideTileText
+    hideTileText = not hideTileText
+
+def Recenter():
+    offset[0] = 0
+    offset[1] = 0
+
+def ExportTilemapToFile(tilemap):
+    
+    output = []
+
+    for row in tilemap.rows:
+        outputRow = []
+        for tile in row:
+            outputRow.append(tile.tileIndex)
+        output.append(outputRow)
+
+    with open("tilemap.txt", "w") as file:
+        file.write("tilemap = [\n")
+        for row in output:
+            if output.index(row) == len(output) - 1:
+                file.write(f"    {row}\n")
+            else:
+                file.write(f"    {row},\n")
+        file.write("]")
+
+# Classes
+
 class Field:
     def __init__(self, x, y, w, h, txt):
         self.fieldRect = pygame.Rect(x, y, w, h)
@@ -62,6 +104,7 @@ class InputField(Field):
         self.inputFieldHighlightRect = pygame.Rect(x - 2, y - 2, w + 4, h + 4)
 
     def Update(self, events):
+        global editing
 
         mousePos = pygame.mouse.get_pos()
 
@@ -75,9 +118,11 @@ class InputField(Field):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if not self.fieldRect.collidepoint(mousePos) and self.active:
                     self.active = False
+                    editing = False
 
                 elif self.fieldRect.collidepoint(mousePos) and not self.active:
                     self.active = True
+                    editing = True
 
             if event.type == pygame.KEYDOWN:
 
@@ -87,6 +132,7 @@ class InputField(Field):
 
                     elif event.key == pygame.K_RETURN:
                         self.active = False
+                        editing = False
 
                     else:
                         if self.txt == self.originalText:
@@ -110,17 +156,46 @@ class InputField(Field):
 
 class BrushField(InputField):
     def __init__(self, x, y, w, h):
-        super().__init__(x, y, w, 32, "0", 4)
+        super().__init__(x, y, w, 32, "0", 4, None, True)
         self.containerRect = pygame.Rect(x, y, w, h)
 
         self.colorOutsideRect = pygame.Rect(x, y + 32 + 4, w, 32)
         self.colorDepthRect = pygame.Rect(x, y + 32 + 4, w, 32)
         self.colorBgRect = pygame.Rect(x + 2, y + 32 + 4 + 2, w - 4, 32 - 4)
 
-    def Update(self, events):
-        pass
+        self.plusBtn = Button(x + 70, y + 2, 22, 22, "+", lambda: self.IncrementBrush())
+        self.minusBtn = Button(x - 28 , y + 2, 22, 22, "-", lambda: self.DecrementBrush())
+
+    def IncrementBrush(self):
+        global currentBrush
+        if currentBrush < 1000:
+            currentBrush += 1
         self.txt = str(currentBrush)
+
+    def DecrementBrush(self):
+        global currentBrush
+        if currentBrush > 0:
+            currentBrush -= 1
+        self.txt = str(currentBrush)
+
+    def Update(self, events):
+        global editing, currentBrush
+
+        super().Update(events)
+
+        #if not editing:
+            #self.txt = str(currentBrush)
+
+        if self.active and self.txt != "":
+            currentBrush = int(self.txt)
+
+        if self.txt == "" and not self.active:
+            self.txt = "0"
+
         self.text = self.font.render(self.txt, True, (0, 0, 0))
+
+        self.plusBtn.Update(events)
+        self.minusBtn.Update(events)
 
     def Draw(self, surface):
         super().Draw(surface)
@@ -130,7 +205,8 @@ class BrushField(InputField):
         pygame.draw.rect(surface, (0, 0, 0), self.colorBgRect, 2)
         pygame.draw.rect(surface, (255, 255, 255), self.colorOutsideRect, 2)
 
-
+        self.plusBtn.Draw(surface)
+        self.minusBtn.Draw(surface)
 
 class Button:
     def __init__(self, x, y, w, h, txt, callback):
@@ -138,12 +214,15 @@ class Button:
         self.normalColor = (198, 198, 198)
         self.seletedColor = (227, 227, 227)
         self.color = self.normalColor
-        self.active = False
+        self.pressed = False
         self.txt = txt
         self.callback = callback
 
         self.shadowRect = pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height + 5)
         self.highlightRect = pygame.Rect(self.rect.x + 2, self.rect.y + 2, self.rect.width - 4, self.rect.height - 4)
+
+        self.font = pygame.font.Font(dogica, 18)
+        self.text = self.font.render(self.txt, True, (0, 0, 0))
 
     def Update(self, events):
         mouse = pygame.mouse.get_pos()
@@ -151,11 +230,16 @@ class Button:
         if self.rect.collidepoint(mouse):
             self.color = self.seletedColor
 
-            if pygame.mouse.get_pressed()[0]:
+            if pygame.mouse.get_pressed()[0] and not self.pressed:
                 self.callback()
+                self.pressed = True
 
         else:
             self.color = self.normalColor
+            self.pressed = False
+
+        if self.pressed and not pygame.mouse.get_pressed()[0]:
+            self.pressed = False
 
     def Draw(self, surface):
         pygame.draw.rect(surface, (self.color[0] - 100, self.color[1] - 100, self.color[2] - 100), self.shadowRect)#shadow
@@ -168,21 +252,20 @@ class Button:
         pygame.draw.rect(surface, (125, 146, 158), (self.rect.x, self.rect.y + self.rect.height - 2, 2, 2))
         pygame.draw.rect(surface, (125, 146, 158), (self.rect.x + self.rect.width - 2, self.rect.y + self.rect.height - 2, 2, 2))
 
-        font = pygame.font.Font(None, 32)
-        text = font.render(self.txt, True, (0, 0, 0))
         #display text on center of button
-        surface.blit(text, (self.rect.x + self.rect.width/2 - text.get_width()/2, self.rect.y + self.rect.height/2 - text.get_height()/2))
+        surface.blit(self.text, (self.rect.x + self.rect.width/2 - self.text.get_width()/2, self.rect.y + self.rect.height/2 - self.text.get_height()/2))
 
 class Tile:
     def __init__(self, x, y, w, h):
         self.rect = pygame.Rect(x, y, w, h)
 
-        self.airColor = (200, 200, 255)
-        self.grassColor = (0, 255, 0)
-        self.dirtColor = (139, 69, 19)
+        self.color = (255, 255, 255)
+        self.borderColor = (self.color[0] - 100, self.color[1] - 100, self.color[2] - 100)
 
-        self.color = self.airColor
-        self.borderColor = (0, 0, 0)
+        self.tileIndex = 0
+
+        self.font = pygame.font.Font(dogica, 32)
+        self.tileText = self.font.render(str(self.tileIndex), True, (0, 0, 0))
 
     def Update(self, events):
         mouse = pygame.mouse.get_pos()
@@ -190,18 +273,27 @@ class Tile:
 
         if self.rect.collidepoint(mouse):
             if pygame.mouse.get_pressed()[0]:
-                if currentBrush == 1:
-                    self.color = self.airColor
+                self.ChangeTile(currentBrush)
 
-                if currentBrush == 2:
-                    self.color = self.grassColor
+    def ChangeTile(self, index):
+        #self.color = self.colors[index]
 
-                if currentBrush == 3:
-                    self.color = self.dirtColor
+        self.tileIndex = index
+        self.tileText = self.font.render(str(index), True, (0, 0, 0))
+        #self.color = colors[currentBrush]
+
+        #tempR = self.color[0] - 100 if self.color[0] - 100 >= 0 else 0
+        #tempG = self.color[1] - 100 if self.color[1] - 100 >= 0 else 0
+        #tempB = self.color[2] - 100 if self.color[2] - 100 >= 0 else 0
+
+        #self.borderColor = (tempR, tempG, tempB)
 
     def Draw(self, surface):
         pygame.draw.rect(surface, self.color, (self.rect.x + offset[0], self.rect.y + offset[1], self.rect.width, self.rect.height))
         pygame.draw.rect(surface, self.borderColor, (self.rect.x + offset[0], self.rect.y + offset[1], self.rect.width, self.rect.height), 2, 0)
+
+        if not hideTileText:
+            surface.blit(self.tileText, (self.rect.x + offset[0] + self.rect.width/2 - self.tileText.get_width()/2, self.rect.y + offset[1] + self.rect.height/2 - self.tileText.get_height()/2))
 
 class Tilemap:
     def __init__(self, w, h):
@@ -256,21 +348,22 @@ class ToolBar:
         self.editArea = editArea
         self.color = (125, 146, 158)
 
-        self.xSizeInput = InputField(16, 16, 64, 32, "x", 4, None, True)
-        self.ySizeInput = InputField(16, 16 + 32 + 16, 64, 32, "y", 4, None, True)
+        #self.xSizeInput = InputField(16, 16, 64, 32, "x", 4, None, True)
+        #self.ySizeInput = InputField(16, 16 + 32 + 16, 64, 32, "y", 4, None, True)
 
-        self.brushField = BrushField(16, 16 + 32 + 16 + 32 + 16, 64, 32+4+32+4+32)
+        self.brushField = BrushField(32, 48, 64, 32+4+32+4+32)
 
-        self.importBtn = Button(16, 256, 64, 64, "im", lambda: print("import"))
-        self.resizeBtn = Button(16, 256+64, 64, 64, "rs", lambda: print("resize"))
-        self.exportTilemapToFileBtn = Button(16, 720-128, 64, 64, "ex", lambda: ExportTilemapToFile(self.editArea.tilemap))
-        self.recenterBtn = Button(16, 720-128-64-64, 64, 64, "rc", lambda: Recenter())
+        self.importBtn = Button(32, 256, 64, 64, "im", lambda: print("import"))
+        self.resizeBtn = Button(32, 256+64, 64, 64, "rs", lambda: print("resize"))
+        self.exportTilemapToFileBtn = Button(32, 720-128, 64, 64, "ex", lambda: ExportTilemapToFile(self.editArea.tilemap))
+        self.recenterBtn = Button(32, 720-128-64-64, 64, 64, "rc", lambda: Recenter())
+        self.toggleHideTileTextBtn = Button(32, 720-128-64-64-64, 64, 64, "ht", ToggleHideTileText)
 
-        self.field = Field(16, 720-128-64-64-64-64, 64, 32, "Field")
+        self.field = Field(32, 720-128-64-64-64-64, 64, 32, "Field")
 
     def Update(self, events):
-        self.xSizeInput.Update(events)
-        self.ySizeInput.Update(events)
+        #self.xSizeInput.Update(events)
+        #self.ySizeInput.Update(events)
 
         self.brushField.Update(events)
 
@@ -278,11 +371,12 @@ class ToolBar:
         self.resizeBtn.Update(events)
         self.exportTilemapToFileBtn.Update(events)
         self.recenterBtn.Update(events)
+        self.toggleHideTileTextBtn.Update(events)
 
     def Draw(self, surface):
         self.surface.fill(self.color)
-        self.xSizeInput.Draw(self.surface)
-        self.ySizeInput.Draw(self.surface)
+        #self.xSizeInput.Draw(self.surface)
+        #self.ySizeInput.Draw(self.surface)
 
         self.brushField.Draw(self.surface)
 
@@ -290,8 +384,9 @@ class ToolBar:
         self.resizeBtn.Draw(self.surface)
         self.exportTilemapToFileBtn.Draw(self.surface)
         self.recenterBtn.Draw(self.surface)
+        self.toggleHideTileTextBtn.Draw(self.surface)
 
-        self.field.Draw(self.surface)
+        #self.field.Draw(self.surface)
 
         surface.blit(self.surface, (0, 0))
 
@@ -377,7 +472,7 @@ class ScrollBar:
         pygame.draw.rect(surface, self.color, self.rect)
         pygame.draw.rect(surface, (255, 255, 255), self.handle, 0, 10)
 
-# Game Loop
+# app loop game loop appl olplp Loop
 
 group = Group()
 
@@ -386,34 +481,6 @@ group.Add(editArea)
 toolBar = ToolBar(editArea)
 group.Add(toolBar)
 
-def Recenter():
-    offset[0] = 0
-    offset[1] = 0
-
-def ExportTilemapToFile(tilemap):
-    
-    output = []
-
-    for row in tilemap.rows:
-        outputRow = []
-        for tile in row:
-            if tile.color == tile.airColor:
-                outputRow.append(0)
-            if tile.color == tile.grassColor:
-                outputRow.append(1)
-            if tile.color == tile.dirtColor:
-                outputRow.append(2)
-        output.append(outputRow)
-
-    with open("tilemap.txt", "w") as file:
-        file.write("tilemap = [\n")
-        for row in output:
-            if output.index(row) == len(output) - 1:
-                file.write(f"    {row}\n")
-            else:
-                file.write(f"    {row},\n")
-        file.write("]")
-
 running = True
 while running:
     events = pygame.event.get()
@@ -421,15 +488,39 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                currentBrush = 1
+        #if event.type == pygame.KEYDOWN:
 
-            if event.key == pygame.K_2:
-                currentBrush = 2
+            # if not editing:
 
-            if event.key == pygame.K_3:
-                currentBrush = 3
+            #     if event.key == pygame.K_0:
+            #         currentBrush = 0
+
+            #     if event.key == pygame.K_1:
+            #         currentBrush = 1
+
+            #     if event.key == pygame.K_2:
+            #         currentBrush = 2
+
+            #     if event.key == pygame.K_3:
+            #         currentBrush = 3
+
+            #     if event.key == pygame.K_4:
+            #         currentBrush = 4
+
+            #     if event.key == pygame.K_5:
+            #         currentBrush = 5
+
+            #     if event.key == pygame.K_6:
+            #         currentBrush = 6
+
+            #     if event.key == pygame.K_7:
+            #         currentBrush = 7
+
+            #     if event.key == pygame.K_8:
+            #         currentBrush = 8
+
+            #     if event.key == pygame.K_9:
+            #         currentBrush = 9
 
         if event.type == pygame.MOUSEMOTION:
             if event.buttons[1]:
